ebfaab2500917ee347c32a4ff95a425c081b0567
mus042 <oshri.mos@gmail.com>
2023-11-27 09:47:43 +0200

    fixed bugs

--- frontend/frontend/app/context/AuthContext.tsx
+++ frontend/frontend/app/context/AuthContext.tsx
@@ -34,7 +34,7 @@ interface AuthProps {
 }
 
 const TOKEN_KEY = "my-jwt";
-export const API_URL = "http://172.26.144.1:3000/";
+export const API_URL = "http://10.0.0.11:3000/";
 const AuthContext = createContext<AuthProps>({});
 
 export const userAuth = () => {
